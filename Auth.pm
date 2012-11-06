3a1c2a678967d3826e400b6d14961670d423aca4
Angelo Naselli <anaselli@mageia.org>
2012-11-06 13:22:23 +0000

    fixed project name

--- Auth.pm
+++ Auth.pm
@@ -1,19 +1,19 @@
 #    Copyright 2012 Matteo Pasotti
 #
-#    This file is part of mcc2
+#    This file is part of AdminPanel
 #
-#    mcc2 is free software: you can redistribute it and/or modify
+#    AdminPanel is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 2 of the License, or
 #    (at your option) any later version.
 #
-#    mcc2 is distributed in the hope that it will be useful,
+#    AdminPanel is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
 #
 #    You should have received a copy of the GNU General Public License
-#    along with mcc2.  If not, see <http://www.gnu.org/licenses/>.
+#    along with AdminPanel.  If not, see <http://www.gnu.org/licenses/>.
 
 package Auth;
 
