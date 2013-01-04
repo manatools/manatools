a887863a4463180b14179583a511481aeab7036d
Angelo Naselli <anaselli@mageia.org>
2013-01-04 10:07:02 +0000

    Forgot apanel.pl

--- extras/create_link.sh
+++ extras/create_link.sh
@@ -12,9 +12,15 @@ then
    rm /usr/share/polkit-1/actions/org.mageia.policykit.pkexec.adminpanel.policy
 fi
 
+if [ -f /usr/bin/apanel.pl ]
+then
+   rm usr/bin/apanel.pl
+fi
+
 pushd .
 cd ..
 cp extras/org.mageia.policykit.pkexec.adminpanel.policy /usr/share/polkit-1/actions/
 ln -s $PWD/AdminPanel `rpm --eval %perl_privlib`
+ln -s $PWD/apanel.pl /usr/bin
 popd
 
