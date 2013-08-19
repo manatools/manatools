9707241dcb48622a47a26f4cc222ca6dfa2a4842
Matteo Pasotti <matteo.pasotti@gmail.com>
2013-08-19 17:05:16 +0200

    - improved installation/uninstallation script (for developers only) - only root should be able to use this script

--- extras/create_link.sh
+++ extras/create_link.sh
@@ -2,25 +2,60 @@
 
 apanel=`rpm --eval %perl_privlib`/AdminPanel
 
-if [ -L $apanel ]
-then
-   rm $apanel
-fi
+function check_root_permissions
+{
+	if [[ $EUID -ne 0 ]]; then
+           echo "You must be root to run this script" 1>&2
+	   exit 1
+	fi
+}
 
-if [ -f /usr/share/polkit-1/actions/org.mageia.policykit.pkexec.adminpanel.policy ]
-then
-   rm /usr/share/polkit-1/actions/org.mageia.policykit.pkexec.adminpanel.policy
-fi
+function uninstall
+{
+   echo "== Uninstalling AdminPanel..."
+   if [ -L $apanel ]
+   then
+      unlink $apanel
+   fi
+   
+   if [ -f /usr/share/polkit-1/actions/org.mageia.policykit.pkexec.adminpanel.policy ]
+   then
+      rm /usr/share/polkit-1/actions/org.mageia.policykit.pkexec.adminpanel.policy
+   fi
+   
+   if [ -f /usr/bin/apanel.pl ]
+   then
+      unlink /usr/bin/apanel.pl
+   fi
+   echo "== Removed"
+}
 
-if [ -f /usr/bin/apanel.pl ]
-then
-   rm /usr/bin/apanel.pl
-fi
+function setup {
+   echo "== Installing AdminPanel..."
+   pushd .
+      cd ..
+      cp extras/org.mageia.policykit.pkexec.adminpanel.policy /usr/share/polkit-1/actions/
+      ln -s $PWD/AdminPanel `rpm --eval %perl_privlib`
+      ln -s $PWD/apanel.pl /usr/bin
+   popd
+   echo "== Done"
+}
 
-pushd .
-cd ..
-cp extras/org.mageia.policykit.pkexec.adminpanel.policy /usr/share/polkit-1/actions/
-ln -s $PWD/AdminPanel `rpm --eval %perl_privlib`
-ln -s $PWD/apanel.pl /usr/bin
-popd
+function usage {
+	echo "Usage:"
+	echo "--remove   uninstall AdminPanel references"
+	echo "--install  install AdminPanel references"
+}
+
+check_root_permissions
+
+while getopts "hri" OPTIONS
+do
+	case $OPTIONS in
+		r  ) uninstall ;;
+		i  ) uninstall && setup ;;
+		h  ) usage ;;
+		\? ) usage ;;
+	esac
+done
 
