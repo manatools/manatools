3b1d4b65b5fa800b3cdfa0e7f6faba6bd7c456ed
Matteo Pasotti <matteo.pasotti@gmail.com>
2014-01-11 15:05:46 +0100

    - added hosts module

---
+++ AdminPanel/Hosts/hosts.pm
@@ -0,0 +1,12 @@
+package AdminPanel::Hosts::hosts; 
+
+use Modern::Perl 2011;
+use autodie;
+use diagnostics;
+# use Config::Hosts;
+
+use base qw(Exporter);
+our @EXPORT = qw(
+                );
+
+1;
