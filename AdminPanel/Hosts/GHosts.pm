69cfb584e95d150fa474b194c2e2d2dcc85d8fa8
Matteo Pasotti <matteo.pasotti@gmail.com>
2014-01-25 00:32:14 +0100

    - updated license headers

--- AdminPanel/Hosts/GHosts.pm
+++ AdminPanel/Hosts/GHosts.pm
@@ -1,7 +1,7 @@
 # vim: set et ts=4 sw=4:
 #*****************************************************************************
 # 
-#  Copyright (c) 2013 Matteo Pasotti <matteo.pasotti@gmail.com>
+#  Copyright (c) 2013-2014 Matteo Pasotti <matteo.pasotti@gmail.com>
 # 
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License version 2, as
