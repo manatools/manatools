74ecfd9fbd5821c44c3c4a2ef4614dc1b82229e8
Angelo Naselli <anaselli@linux.it>
2014-04-15 12:46:01 +0200

    Fixed POD

--- lib/AdminPanel/Module/Services.pm
+++ lib/AdminPanel/Module/Services.pm
@@ -26,7 +26,7 @@ AdminPanel::Module::Services - This module aims to manage service
 
     You can find documentation for this module with the perldoc command:
 
-    perldoc AdminPanel::Module::Services::Module
+    perldoc AdminPanel::Module::Services
 
 =head1 SEE ALSO
    
@@ -52,8 +52,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
-
-=head1 FUNCTIONS
 
 =cut
 
@@ -165,6 +163,9 @@ Version 1.0.0
 
 our $VERSION = '1.0.0';
 
+=head1 METHODS
+
+=cut
 
 #=============================================================
 
