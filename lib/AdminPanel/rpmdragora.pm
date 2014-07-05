bd2d821325a1d7deefac9bc2a5030dd290aa8901
Angelo Naselli <anaselli@linux.it>
2014-07-06 00:17:39 +0200

    wrong member called

--- lib/AdminPanel/rpmdragora.pm
+++ lib/AdminPanel/rpmdragora.pm
@@ -652,7 +652,7 @@ sub update_sources {
         callback => sub {
             $cancel and goto cancel_update;
             my ($type, $media) = @_;
-            return if $type !~ /^(?:start|progress|end)$/ && @media && !AdminPanel::Shared::member($media, @media);
+            return if $type !~ /^(?:start|progress|end)$/ && @media && !member($media, @media);
             if ($type eq 'failed') {
                 $urpm->{fatal}->(N("Error retrieving packages"),
 N("It's impossible to retrieve the list of new packages from the media
