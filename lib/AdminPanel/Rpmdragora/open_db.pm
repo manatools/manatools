a46751b036fe1d06f35122296c49543796b58643
Angelo Naselli <anaselli@linux.it>
2014-12-11 15:12:48 +0100

    defualt is empty string not undefined

--- lib/AdminPanel/Rpmdragora/open_db.pm
+++ lib/AdminPanel/Rpmdragora/open_db.pm
@@ -76,7 +76,7 @@ sub open_rpm_db {
 	    $db = new URPM;
             $db->parse_synthesis("$::env/rpmdb.cz");
 	} else {
-            $db = URPM::DB::open($::rpmdragora_options{'rpm-root'}[0]);
+            $db = URPM::DB::open($::rpmdragora_options{'rpm-root'}[0]||'');
         }
         $db or die "Couldn't open RPM DB (" . ($::env ? "$::env/rpmdb.cz" : $::rpmdragora_options{'rpm-root'}[0]) . ")";
     }
