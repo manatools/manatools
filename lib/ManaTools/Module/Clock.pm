b33f2b764a47124aad1c696c5ef1d31df762c489
Angelo Naselli <anaselli@linux.it>
2015-03-25 23:20:29 +0100

    Updated

--- lib/ManaTools/Module/Clock.pm
+++ lib/ManaTools/Module/Clock.pm
@@ -111,11 +111,11 @@ sub _SharedTimeZoneInitialize {
 
 =head1 VERSION
 
-Version 1.0.0
+Version 1.0.1
 
 =cut
 
-our $VERSION = '1.0.0';
+our $VERSION = '1.0.1';
 
 #=============================================================
 
