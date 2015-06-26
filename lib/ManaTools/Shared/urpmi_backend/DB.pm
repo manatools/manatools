505be0a8194a8e68492ef7e7d28dda62c6873153
Angelo Naselli <anaselli@linux.it>
2015-06-26 21:45:42 +0200

    fixed documentation

--- lib/ManaTools/Shared/urpmi_backend/DB.pm
+++ lib/ManaTools/Shared/urpmi_backend/DB.pm
@@ -1,29 +1,61 @@
 # vim: set et ts=4 sw=4:
 package ManaTools::Shared::urpmi_backend::DB;
-#*****************************************************************************
-#
-#  Copyright (c) 2015 Angelo Naselli <anaselli@linux.it>
-#  from Rpmdrake::open_db:
-#       Copyright (c) 2002 Guillaume Cottenceau
-#       Copyright (C) 2008 Aurelien Lefebvre <alkh@mandriva.org>
-#       Copyright (c) 2002-2014 Thierry Vignaud <thierry.vignaud@gmail.com>
-#       Copyright (c) 2003, 2004, 2005 MandrakeSoft SA
-#       Copyright (c) 2005-2007 Mandriva SA
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License version 2, as
-#  published by the Free Software Foundation.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-#
-#*****************************************************************************
+
+#============================================================= -*-perl-*-
+
+=head1 NAME
+
+    ManaTools::Shared::urpmi_backend::DB - urpm DB object
+
+=head1 SYNOPSIS
+
+    use ManaTools::Shared::urpmi_backend::DB;
+
+    my $db_man = ManaTools::Shared::urpmi_backend::DB->new();
+    my $urpm = $db_man->fast_open_urpmi_db();
+
+
+=head1 DESCRIPTION
+
+    This module is a backend to urpm open db
+
+=head1 SUPPORT
+
+    You can find documentation for this module with the perldoc command:
+
+    perldoc ManaTools::Shared::urpmi_backend::DB
+
+
+=head1 AUTHOR
+
+    Angelo Naselli <anaselli@linux.it>
+
+=head1 COPYRIGHT and LICENSE
+
+Copyright (c) 2015 Angelo Naselli <anaselli@linux.it>
+    from Rpmdrake::open_db:
+     Copyright (c) 2002 Guillaume Cottenceau
+     Copyright (C) 2008 Aurelien Lefebvre <alkh@mandriva.org>
+     Copyright (c) 2002-2014 Thierry Vignaud <thierry.vignaud@gmail.com>
+     Copyright (c) 2003, 2004, 2005 MandrakeSoft SA
+     Copyright (c) 2005-2007 Mandriva SA
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 2, as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+=head1 METHODS
+
+=cut
 
 use Moose;
 
@@ -291,7 +323,7 @@ sub lock {
 
 #=============================================================
 
-=head2 lock
+=head2 unlock
 
 =head3 INPUT
 
